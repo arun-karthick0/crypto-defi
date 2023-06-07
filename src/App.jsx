e2aaa8552036b463fab4961215f922d61efd4d67
Arun <arunkarthick0810@gmail.com>
2023-06-07 10:31:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Featured from "./component/Featured";
+import Hero from "./component/Footer";
+import Navbar from "./component/Hero";
+import Sign from "./component/Navbar";
+import Footer from "./component/Signup";
+
+function App() {
+  return (
+    <>
+      <Sign />
+      <Navbar />
+      <Featured />
+      <Footer />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
